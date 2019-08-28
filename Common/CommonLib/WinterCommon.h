5d3c676942dd9c2a78712c2d2ab2f7e7963f0a5f
lizp <lizp@nbchat.com>
2019-08-28 16:45:12 +0800

    添加公共引入类

---
+++ Common/CommonLib/WinterCommon.h
@@ -0,0 +1,24 @@
+//
+//  NBCommonLib.h
+//  Common
+//
+//  Created by Li Zhiping on 2018/8/7.
+//  Copyright © 2018 Lizhiping. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "RACEXTScope.h"
+#import "TTGlobalUICommon.h"
+#import "NBActionSheet.h"
+#import "NBAlertView.h"
+#import "Colours.h"
+#import "XDNibCellObject.h"
+#import "XDNibInputCellObject.h"
+#import "XDSectionTitleObject.h"
+#import "XDCellCatalog.h"
+
+#ifndef MAS_SHORTHAND_GLOBALS
+    #define MAS_SHORTHAND_GLOBALS
+#endif
