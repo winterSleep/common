a6115a1c1848878e83464aa58fa69d56dc1fa001
lizp <lizp@nbchat.com>
2019-08-28 11:11:50 +0800

    删除下拉刷新代码

--- Common/CommonLib/BaseClass/Controller/NBTableViewController.m
+++ Common/CommonLib/BaseClass/Controller/NBTableViewController.m
@@ -8,11 +8,6 @@
 
 #import "NBTableViewController.h"
 #import <Masonry/Masonry.h>
-#import "SVPullToRefresh.h"
-#import "NBPullToRefreshLoadingView.h"
-#import "NBPullToRefreshTriggerView.h"
-#import "NBPullToRefreshStopView.h"
-#import "NBPullToRefreshBaseView.h"
 
 @interface NBTableViewController ()
 {
@@ -51,49 +46,6 @@
     [self.tableView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.edges.equalTo(self.view);
     }];
-    //初始化下拉刷新和上拉加载更多操作
-    __weak NBTableViewController *weakSelf = self;
-    [self.tableView zy_addPullToRefreshWithActionHandler:^(BOOL isTriggered) {
-        if (isTriggered) {
-            [weakSelf triggerPullRefreshAction];
-        }else{
-            [weakSelf pullToRefreshAction];
-        }
-    }];
-    [self.tableView setZy_showsPullToRefresh:NO];
-    
-    [self.tableView zy_addInfiniteScrollingWithActionHandler:^{
-        [weakSelf infiniteScrollingAction];
-    }];
-    [self.tableView setZy_showsInfiniteScrolling:NO];
-    
-    NBPullToRefreshLoadingView *refreshLoadingView = [[NBPullToRefreshLoadingView alloc] initWithFrame:CGRectZero];
-    [self.tableView.zy_pullToRefreshView setCustomView:refreshLoadingView forState:ZYSVPullToRefreshStateLoading];
-    
-    NBPullToRefreshTriggerView *refreshTriggerView = [[NBPullToRefreshTriggerView alloc] initWithFrame:CGRectZero];
-    [self.tableView.zy_pullToRefreshView setCustomView:refreshTriggerView forState:ZYSVPullToRefreshStateTriggered];
-    
-    NBPullToRefreshStopView *refreshStopView = [[NBPullToRefreshStopView alloc] initWithFrame:CGRectZero];
-    [self.tableView.zy_pullToRefreshView setCustomView:refreshStopView forState:ZYSVPullToRefreshStateStopped];
-    
-    [self.tableView.zy_infiniteScrollingView setCustomView:refreshLoadingView forState:ZYSVInfiniteScrollingStateLoading];
-    
-    [self.tableView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
-    [self.tableView addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionNew context:nil];
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
-    if (object == self.tableView) {
-        if ([keyPath isEqualToString:@"frame"] ||
-            [keyPath isEqualToString:@"contentOffset"]) {
-            NBPullToRefreshBaseView *view = (NBPullToRefreshBaseView *)[self.tableView.zy_pullToRefreshView customViewForState:ZYSVPullToRefreshStateLoading];
-            [view scrollViewDidScroll:self.tableView];
-            view = (NBPullToRefreshBaseView *)[self.tableView.zy_pullToRefreshView customViewForState:ZYSVPullToRefreshStateTriggered];
-            [view scrollViewDidScroll:self.tableView];
-            view = (NBPullToRefreshBaseView *)[self.tableView.zy_pullToRefreshView customViewForState:ZYSVPullToRefreshStateStopped];
-            [view scrollViewDidScroll:self.tableView];
-        }
-    }
 }
 
 - (UITableView *)tableView
@@ -131,12 +83,7 @@
 #pragma mark - TableViewDelegate
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
-    NBPullToRefreshBaseView *view = (NBPullToRefreshBaseView *)[self.tableView.zy_pullToRefreshView customViewForState:ZYSVPullToRefreshStateLoading];
-    [view scrollViewWillBeginDragging:self.tableView];
-    view = (NBPullToRefreshBaseView *)[self.tableView.zy_pullToRefreshView customViewForState:ZYSVPullToRefreshStateTriggered];
-    [view scrollViewWillBeginDragging:self.tableView];
-    view = (NBPullToRefreshBaseView *)[self.tableView.zy_pullToRefreshView customViewForState:ZYSVPullToRefreshStateStopped];
-    [view scrollViewWillBeginDragging:self.tableView];
+    
 }
 
 #pragma mark - Table view data source
@@ -165,17 +112,24 @@
 
 #pragma mark -
 
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc{
+    _tableView = nil;
+}
+
+#pragma mark - deprecated
+
 - (void)setShowsPullToRefresh:(BOOL)showsPullToRefresh{
-    self.isShowsPullToRefresh = showsPullToRefresh;
     
-    [self.tableView setZy_showsPullToRefresh:showsPullToRefresh];
 }
 
 - (void)triggerPullToRefresh{
-    [self.tableView scrollRectToVisible:CGRectMake(0, 0, 1, 1) animated:YES];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self.tableView zy_triggerPullToRefresh];
-    });
+    
 }
 
 - (void)pullToRefreshAction{
@@ -190,25 +144,21 @@
 }
 
 - (void)stopPullAnnimation{
-    if (self.isShowsPullToRefresh && [self isPullAnimation]) {
-        [self.tableView.zy_pullToRefreshView stopAnimating];
-    }
+    
 }
 
 - (BOOL)isPullAnimation{
-    return self.tableView.zy_pullToRefreshView.state == ZYSVPullToRefreshStateLoading;
+    return false;
 }
 
 - (BOOL)isInfiniteAnimation{
-    return self.tableView.zy_infiniteScrollingView.state == ZYSVInfiniteScrollingStateLoading;
+    return false;
 }
 
 #pragma mark -
 
 - (void)setShowsInfiniteScrolling:(BOOL)showsInfiniteScrolling{
-    self.isShowsInfiniteScrolling = showsInfiniteScrolling;
     
-    [self.tableView setZy_showsInfiniteScrolling:showsInfiniteScrolling];
 }
 
 - (void)infiniteScrollingAction{
@@ -216,23 +166,7 @@
 }
 
 - (void)stopInfiniteAnimation{
-    if ([self isInfiniteAnimation]) {
-        [self.tableView.zy_infiniteScrollingView stopAnimating];
-    }
-}
-
-#pragma mark -
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)dealloc{
-    [_tableView removeObserver:self forKeyPath:@"contentOffset"];
-    [_tableView removeObserver:self forKeyPath:@"frame"];
-    _tableView = nil;
+    
 }
 
 @end
