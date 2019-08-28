26fa7c5c8888c79d1a605b10dc2b7d1e37e360a8
lizp <lizp@nbchat.com>
2019-08-28 16:36:25 +0800

    添加部分header引入

--- Common/CommonLib/NBCommonLib.h
+++ Common/CommonLib/NBCommonLib.h
@@ -7,6 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "RACEXTScope.h"
+#import "TTGlobalUICommon.h"
+#import "NBActionSheet.h"
+#import "NBAlertView.h"
+#import "Colours.h"
+#import "XDNibCellObject.h"
+#import "XDNibInputCellObject.h"
+#import "XDSectionTitleObject.h"
+#import "XDCellCatalog.h"
 
 #ifndef MAS_SHORTHAND_GLOBALS
     #define MAS_SHORTHAND_GLOBALS
